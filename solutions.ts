b99262f00ebebeec4a64235269e830c3b8687fa2
Purnendu-sarkar <dsr102.purnendu@gmail.com>
2025-05-08 12:06:59 +0600

    Complete Problems 02

--- solutions.ts
+++ solutions.ts
@@ -1,8 +1,27 @@
+// Problem 1
 function formatString(input: string, toUpper: boolean = true): string {
   return toUpper === true ? input.toUpperCase() : input.toLowerCase();
 }
 console.log(formatString("Hello")); 
 console.log(formatString("Hello", true)); 
 console.log(formatString("Hello", false)); 
+
+// Problem 2
+function filterByRating(
+  items: { title: string; rating: number }[]
+): { title: string; rating: number }[] {
+  return items.filter((item) => item.rating >= 4);
+}
+
+const books = [
+  { title: "Book A", rating: 4.5 },
+  { title: "Book B", rating: 3.2 },
+  { title: "Book C", rating: 5.0 },
+];
+
+console.log(filterByRating(books));
+
+
+
 
 
